d947954a15aa081443884a438df4b5a7809a9355
Seth Lachman <lachmanseth17@gmail.com>
2019-10-19 11:39:46 -0400

    Completed registration endpoint.

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 
-const authRouter = require("./auth/auth-router");
+const authRouter = require("../auth/auth-router.js");
 
 const server = express();
 
